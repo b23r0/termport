c2cd51d8a22d79243c9867dfba1f50baae9fa60b
b23r0 <b23r0@foxmail.com>
2022-07-23 12:35:08 +0800

    fix close lock && add set sub title

--- example/egui_example/src/main.rs
+++ example/egui_example/src/main.rs
@@ -12,11 +12,13 @@ fn main() {
 }
 
 struct MyApp {
+    term : Option<TermInstance>
 }
 
 impl Default for MyApp {
     fn default() -> Self {
         Self {
+            term : None
         }
     }
 }
@@ -25,12 +27,12 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             if ui.button("start terminal").clicked() {
-                let mut term = new_term(&"alacritty_driver.exe".to_string()).unwrap();
+                self.term = Some(new_term(&"alacritty_driver.exe".to_string(), &"sub_title".to_string()).unwrap());
                 let buf = "\x1b[0;31mHello Termport\x1b[0m".as_bytes();
-                term.write(buf).unwrap();
-                std::thread::spawn(move || {
-                    term.wait_for_exit().unwrap();
-                });
+                self.term.as_mut().unwrap().write(buf).unwrap();
+            }
+            if ui.button("close").clicked() {
+                self.term.as_ref().unwrap().close().unwrap();
             }
         });
     }
--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,12 @@ use std::sync::{Mutex, Arc};
 
 pub struct TermInstance{
     socket: TcpStream,
+    pid : u32,
     cmd : Arc<Mutex<Child>>
 }
 
 impl TermInstance{
-    pub fn new(driver_path : &String) -> Result<TermInstance>{
+    pub fn new(driver_path : &String , sub_title : &String ) -> Result<TermInstance>{
 
         let server = TcpListener::bind("127.0.0.1:0")?;
         let local_socket_port = format!("{}" , server.local_addr().unwrap().port());
@@ -17,11 +18,18 @@ impl TermInstance{
         let cmd = if cfg!(target_os = "windows") {
             let mut cmd = Command::new(driver_path);
             cmd.args(["--local-socket-port", local_socket_port.as_str()]);
+            if !sub_title.is_empty(){
+                cmd.args(["--sub-title", sub_title.as_str()]);
+            }
             cmd.spawn()
         } else {
             let mut cmd = Command::new(driver_path);
             cmd.arg("--local-socket-port");
             cmd.arg(local_socket_port.as_str());
+            if !sub_title.is_empty(){
+                cmd.arg("--sub-title");
+                cmd.arg(sub_title);
+            }
             cmd.spawn()
         }?;
 
@@ -29,6 +37,7 @@ impl TermInstance{
 
         Ok(TermInstance{
             socket,
+            pid : cmd.id(),
             cmd : Arc::new(Mutex::new(cmd))
         })
     }
@@ -42,25 +51,36 @@ impl TermInstance{
     pub fn wait_for_exit(&mut self) -> Result<ExitStatus>{
         self.cmd.lock().unwrap().wait()
     }
-    pub fn close(&mut self) -> Result<()>{
-        self.cmd.lock().unwrap().kill()
+    pub fn close(&self) -> Result<()>{
+        if cfg!(target_os = "windows") {
+            let mut cmd = Command::new("taskkill");
+            cmd.args(["/F"]);
+            cmd.args(["/pid", self.pid.to_string().as_str()]);
+            cmd.spawn()
+        } else {
+            let mut cmd = Command::new("kill");
+            cmd.arg("-9");
+            cmd.arg(self.pid.to_string().as_str());
+            cmd.spawn()
+        }?;
+        Ok(())
     }
 
 }
 
-pub fn new_term(driver_path : &String) -> Result<TermInstance>{
-    TermInstance::new(driver_path)
+pub fn new_term(driver_path : &String , sub_title : &String) -> Result<TermInstance>{
+    TermInstance::new(driver_path , sub_title)
 }
 
 impl Clone for TermInstance{
     fn clone(&self) -> Self {
-        Self { socket: self.socket.try_clone().unwrap(), cmd: self.cmd.clone() }
+        Self { socket: self.socket.try_clone().unwrap(), cmd: self.cmd.clone(), pid: self.pid }
     }
 }
 
 #[test]
 fn test() {
-    let mut term = new_term(&"alacritty_driver.exe".to_string()).unwrap();
+    let mut term = new_term(&"alacritty_driver.exe".to_string(), &"".to_string()).unwrap();
     let buf = "\x1b[0;31mHello Termport\x1b[0m".as_bytes();
     term.write(buf).unwrap();
     term.wait_for_exit().unwrap();
